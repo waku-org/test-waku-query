7cdd7e4fad93bc0a21befc45106568e4e88192f2
Ivan Folgueira Bande <ivansete@status.im>
2023-08-25 10:27:41 +0200

    Setting storenode counter parts
    With this parameter, each node is configured as "store client" and the
    "store server" is the one indicated in the "storenode" parameter.

--- docker/run_node_postgres.sh
+++ docker/run_node_postgres.sh
@@ -8,6 +8,7 @@ peer_IP=$(dig +short nwaku_sqlite)
 exec /usr/bin/wakunode\
   --nodekey=1d714a1fada214dead6dc9c7274585eca0ff292451866e7d6d677dc818e8ccd2\
   --staticnode=/ip4/${peer_IP}/tcp/30304/p2p/16Uiu2HAkxj3WzLiqBximSaHc8wV9Co87GyRGRYLVGsHZrzi3TL5W\
+  --storenode=/ip4/${peer_IP}/tcp/30304/p2p/16Uiu2HAkxj3WzLiqBximSaHc8wV9Co87GyRGRYLVGsHZrzi3TL5W\
   --relay=true\
   --topic=/waku/2/default-waku/proto\
   --topic=/waku/2/dev-waku/proto\
--- docker/run_node_sqlite.sh
+++ docker/run_node_sqlite.sh
@@ -8,6 +8,7 @@ peer_IP=$(dig +short nwaku_postgres)
 exec /usr/bin/wakunode\
   --nodekey=2d714a1fada214dead6dc9c7274585eca0ff292451866e7d6d677dc818e8ccd2\
   --staticnode=/ip4/${peer_IP}/tcp/30303/p2p/16Uiu2HAmJyLCRhiErTRFcW5GKPrpoMjGbbWdFMx4GCUnnhmxeYhd\
+  --storenode=/ip4/${peer_IP}/tcp/30303/p2p/16Uiu2HAmJyLCRhiErTRFcW5GKPrpoMjGbbWdFMx4GCUnnhmxeYhd\
   --relay=true\
   --topic=/waku/2/default-waku/proto\
   --topic=/waku/2/dev-waku/proto\
