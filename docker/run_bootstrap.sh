2859cae961b7fadb33be4040117dc06cf8ed4e54
Ivan Folgueira Bande <ivansete@status.im>
2023-10-15 20:26:40 +0200

    Connecting nodes with bootstrap node and adding go-waku store clients
    One "go-waku" relay node is responsible for generating
    traffic, which in turns creates "insert" events. And another
    go-waku node that acts as a store client to generate "store"
    requests to both "sqlite" and "postgres" nwaku store nodes.

---
+++ docker/run_bootstrap.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+IP=$(ip a | grep "inet " | grep -Fv 127.0.0.1 | sed 's/.*inet \([^/]*\).*/\1/')
+
+echo "I am a bootstrap node. Listening to: ${IP}"
+
+exec /usr/bin/wakunode\
+      --relay=true\
+      --rpc-admin=true\
+      --keep-alive=true\
+      --max-connections=300\
+      --dns-discovery=true\
+      --discv5-discovery=true\
+      --discv5-enr-auto-update=True\
+      --log-level=INFO\
+      --rpc-port=8544\
+      --rpc-address=0.0.0.0\
+      --metrics-server=True\
+      --metrics-server-address=0.0.0.0\
+      --nodekey=30348dd51465150e04a5d9d932c72864c8967f806cce60b5d26afeca1e77eb68\
+      --nat=extip:${IP}
---
+++ docker/run_waku_store_query_maker.sh
@@ -0,0 +1,58 @@
+#!/bin/sh
+
+IP=$(ip a | grep "inet " | grep -Fv 127.0.0.1 | sed 's/.*inet \([^/]*\).*/\1/')
+
+echo "I am a waku store query generator"
+
+## Getting the address of the Postgres node
+RETRIES=10
+while [ -z "${POSTGRES_ADDR}" ] && [ ${RETRIES} -ge 0 ]; do
+  POSTGRES_ADDR=$(wget -O - --post-data='{"jsonrpc":"2.0","method":"get_waku_v2_debug_v1_info","params":[],"id":1}' --header='Content-Type:application/json' http://nwaku-postgres:8545/ 2> /dev/null | sed 's/.*"listenAddresses":\["\(.*\)"].*/\1/');
+  wget -O - --post-data='{"jsonrpc":"2.0","method":"get_waku_v2_debug_v1_info","params":[],"id":1}' --header='Content-Type:application/json' http://nwaku-postgres:8545/
+  echo "Store Postgres node node not ready, retrying (retries left: ${RETRIES})"
+  sleep 1
+  RETRIES=$(( $RETRIES - 1 ))
+done
+
+if [ -z "${POSTGRES_ADDR}" ]; then
+   echo "Could not get POSTGRES_ADDR and none provided. Failing"
+   exit 1
+fi
+
+## Getting the address of the SQLite node
+RETRIES=10
+while [ -z "${SQLITE_ADDR}" ] && [ ${RETRIES} -ge 0 ]; do
+  SQLITE_ADDR=$(wget -O - --post-data='{"jsonrpc":"2.0","method":"get_waku_v2_debug_v1_info","params":[],"id":1}' --header='Content-Type:application/json' http://nwaku-sqlite:8546/ 2> /dev/null | sed 's/.*"listenAddresses":\["\(.*\)"].*/\1/');
+  echo "Store SQLite node node not ready, retrying (retries left: ${RETRIES})"
+  sleep 1
+  RETRIES=$(( $RETRIES - 1 ))
+done
+
+if [ -z "${SQLITE_ADDR}" ]; then
+   echo "Could not get SQLITE_ADDR and none provided. Failing"
+   exit 1
+fi
+
+## Getting the bootstrap node ENR
+RETRIES=10
+while [ -z "${BOOTSTRAP_ENR}" ] && [ ${RETRIES} -ge 0 ]; do
+  BOOTSTRAP_ENR=$(wget -O - --post-data='{"jsonrpc":"2.0","method":"get_waku_v2_debug_v1_info","params":[],"id":1}' --header='Content-Type:application/json' http://bootstrap:8544/ 2> /dev/null | sed 's/.*"enrUri":"\([^"]*\)".*/\1/');
+  echo "Bootstrap node not ready, retrying (retries left: ${RETRIES})"
+  sleep 1
+  RETRIES=$(( $RETRIES - 1 ))
+done
+
+if [ -z "${BOOTSTRAP_ENR}" ]; then
+   echo "Could not get BOOTSTRAP_ENR and none provided. Failing"
+   exit 1
+fi
+
+echo "Using bootstrap node: ${BOOTSTRAP_ENR}"
+exec /main\
+    --pubsub-topic="/waku/2/default-waku/proto"\
+    --content-topic="my-ctopic"\
+    --queries-per-second=${STORE_QUERIES_PER_SECOND}\
+    --bootstrap-node=${BOOTSTRAP_ENR}\
+    --peer-store-postgres-addr="${POSTGRES_ADDR}"\
+    --peer-store-sqlite-addr="${SQLITE_ADDR}"\
+    --num-minutes-query=60
