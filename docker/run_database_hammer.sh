c824a8018e52bb58704f776a933558ae0d9440ed
Ivan Folgueira Bande <ivansete@status.im>
2023-11-22 23:30:21 +0100

    simplification of database hammer code base

--- docker/run_database_hammer.sh
+++ docker/run_database_hammer.sh
@@ -3,23 +3,20 @@
 ## This script is aimed to generate traffic to the database and simulate
 ## simulate multiple Waku nodes connected to the same database.
 
-## Install the `dig` command
-apk --no-cache add bind-tools
-
 ## Install postgresql-client for psql command
 apk --no-cache add postgresql-client
 
 ## The port numbers and host names are defined in the 'docker-compose.yml' file
-nwaku-postgres-IP=$(dig +short postgres)
-target-postgres="http://${nwaku-postgres-IP}:5432"
 
-echo "This is publisher: ${target-postgres}; ${target_sqlite}"
+echo "This is database hammer"
 
-query-last-five-minutes="SELECT storedAt, contentTopic, payload, pubsubTopic, version, timestamp, id FROM messages WHERE storedAt >= EXTRACT(EPOCH FROM (NOW() - INTERVAL '5 minutes')) * 1000 ORDER BY storedAt DESC;"
+export PGPASSWORD=test123
+QUERY_LAST_FIVE_MINUTES=$(echo 'SELECT * FROM messages ORDER BY storedAt DESC LIMIT 100;')
+
 while true
 do
   echo "Before making query"
-  psql -h ${nwaku-postgres-IP} -U postgres -W test123 -U postgres -c "${query-last-five-minutes}"
+  psql -h postgres -p 5432 -U postgres -U postgres -w -c "${QUERY_LAST_FIVE_MINUTES}"
 
   sleep 10
 done
